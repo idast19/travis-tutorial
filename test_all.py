10a4200c476e0406a41e9d8214a37017247bed71
Isabelle DAST <dast@cerfacs.fr>
2017-05-31 12:17:24 +0200

    add test numpy

--- test_all.py
+++ test_all.py
@@ -1,5 +1,12 @@
+import numpy as np
+
 def test_addition():
     assert 1 + 1 == 2
 
 def test_division():
     assert  3./2 == 1.5
+
+def test_numpy():
+    input_arr= np.array([3,2,-1,10])
+    part = np.partition(input_arr,1)
+    assert part[1] == 2
